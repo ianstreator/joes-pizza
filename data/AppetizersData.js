006b94cb45962792d5e4adaaecb1a07b0b855e40
Ian Streator <istreator@yahoo.com>
2022-07-11 21:09:31 -0500

    created and formatted JSON data for Pizza and Appetizers, other menu categories JSON data WIP

--- data/AppetizersData.js
+++ data/AppetizersData.js
@@ -1,27 +1,77 @@
-const AppetizersData = {
-  Quantity: [`6pc`, `12pc`, `24pc`],
-  Appetizer: [
-    `Jumbo Buffalo Wings`,
-    `Chicken Strips`,
-    `Fried Ravioli`,
-    `Mozzarella Sticks`,
-    `Jalepeno Poppers`,
-    `Onion Rings`,
-    `Calamari`,
-    `Stuffed Mushrooms (6pc)`,
-    `Breaded Cauliflower`,
-    `Breaded Zucchini`,
-    `Breaded Mushrooms`,
-    `Veggie Combo`,
-    `Tour De Joe's`,
-    `Breadsticks`,
-    `Fried Dough Nuggets`,
-    `Garlic Bread`,
-    `Cheezy Garlic Bread`,
-    `Pizza Bread`,
-    `Ultimate Fries`,
-    `French Fries`,
-  ],
+const Appetizers = {
+  Quantity: ["6pc", "12pc", "24pc"],
+  quantityAppetizers: {
+    "Jumbo Buffalo Wings": [
+      ["8.99", "11.99", "22.99"],
+      "Famous meaty wings hand-dipped in your choice of mild, medium, hot BBQ or garlic sauce, served with a side of blue cheese or ranch.",
+    ],
+    "Chicken Strips": [
+      ["7.25", "13.00", "23.00"],
+      "Home-style battered and served regular or buffalo with blue cheese or ranch.",
+    ],
+    "Fried Ravioli": [
+      ["8.00", "14.00", "25.00"],
+      "Fried golden brown and served with our homemade marinara.",
+    ],
+    "Mozzarella Sticks": [
+      ["6.25", "10.25", "18.25"],
+      "Creamy mozzarella breaded and served with marinara.",
+    ],
+    "Jalepeno Poppers": [
+      ["6.75", "11.25", "20.00"],
+      "Spicy Jalapenos stuffed with cream cheese. Served with marinara or ranch.",
+    ],
+  },
+  soloAppetizers: {
+    "Onion Rings": [5.99, "Mound of crispy rings, enough to share."],
+    "Calamari": [
+      9.75,
+      "An Italian classic, succulent squid deep fried to perfection in an Italian-style batter.",
+    ],
+    "Stuffed Mushrooms (6pc)": [
+      9.75,
+      "Fresh mushrooms fried in a tasty golden breading. Crab filling 100%.",
+    ],
+    "Breaded Cauliflower": [
+      6.25,
+      "Delicately nutty, fried until irresistible.",
+    ],
+    "Breaded Zucchini": [
+      6.25,
+      "Sweet garden-fresh zucchini in a golden breading.",
+    ],
+    "Breaded Mushrooms": [
+      6.25,
+      "Fresh mushrooms fried in a tasty golden breading.	",
+    ],
+    "Veggie Combo": [
+      9.25,
+      "Eating your veggies is easy (and tasty) when they're this good! A plentiful mix of mushrooms, cauliflower, and zucchini.	",
+    ],
+    "Tour De Joe's": [
+      11.5,
+      "The perfect appetizer combination! Jumbo buffalo wings, mozzarella sticks, spicy jalapeno poppers, and breaded mushrooms mean something for everyone!	",
+    ],
+    "Breadsticks (4pc / 8pc)": [[3.5, 6.0], "Buttery goodness!"],
+    "Fried Dough Nuggets": [
+      9.75,
+      "Tossed in a garlic parmigiana butter, they're just right the perfect bite.",
+    ],
+    "Garlic Bread": [4.5, "French bread with garlic and butter."],
+    "Cheezy Garlic Bread": [
+      6.25,
+      "French bread topped with garlic, butter, and melted mozzarella.",
+    ],
+    "Pizza Bread": [
+      6.25,
+      "French bread topped with garlic, butter, tomato sauce and real mozzarella.        ",
+    ],
+    "Ultimate Fries": [
+      8.75,
+      'When we say "ultimate" we mean it! These crispy golden wonders are smothered with cheddar cheese and crispy bacon.',
+    ],
+    "French Fries": [3.75, "Crispy and golden brown."],
+  },
 };
 
-export default AppetizersData;
+export default Appetizers;
--- data/PizzaData.js
+++ data/PizzaData.js
@@ -1,48 +1,91 @@
-const PizzaData = {
-  Size: [`10"`, `12"`, `14"`, `16"`, `18"`],
-  Crust: [
-    `X-Thin`,
-    `Thin`,
-    `Sicilian Style Thick`,
-    `Pan Pizza`,
-    `Chicago Style Deep Dish`,
-    `The Original Stuffed Joe`,
-    `Gluten Free 12" Only`,
-  ],
+const BuildYourOwnPizza = {
+  Size: ['10"', '12"', '14"', '16"', '18"'],
+  Crust: {
+    "X-Thin": "",
+    Thin: "",
+    "Sicilian Style Thick": "",
+    "Pan Pizza": "",
+    "Chicago Style Deep Dish": "",
+    "The Original Stuffed Joe": "",
+    'Gluten Free 12" Only': "",
+  },
   Toppings: [
-    `Sausage`,
-    `Green`,
-    `Peppers`,
-    `Fresh`,
-    `Garlic`,
-    `Pepperoni`,
-    `Sliced`,
-    `Tomatoes`,
-    `Hot`,
-    `Giardiniera`,
-    `Ground`,
-    `Beef`,
-    `Spinach`,
-    `Jalapenos`,
-    `Mushrooms`,
-    `Green`,
-    `Olives`,
-    `Cheddar`,
-    `Cheese`,
-    `Onions`,
-    `Black`,
-    `Olives`,
-    `Anchovies`,
-    `Chicken`,
-    `Canadian`,
-    `Bacon`,
-    `Pineapple`,
-    `Shrimp`,
-    `Italian`,
-    `Beef`,
-    `Ricotta`,
-    `Cheese`,
+    "Sausage",
+    "Green",
+    "Peppers",
+    "Fresh",
+    "Garlic",
+    "Pepperoni",
+    "Sliced",
+    "Tomatoes",
+    "Hot",
+    "Giardiniera",
+    "Ground",
+    "Beef",
+    "Spinach",
+    "Jalapenos",
+    "Mushrooms",
+    "Green",
+    "Olives",
+    "Cheddar",
+    "Cheese",
+    "Onions",
+    "Black",
+    "Olives",
+    "Anchovies",
+    "Chicken",
+    "Canadian",
+    "Bacon",
+    "Pineapple",
+    "Shrimp",
+    "Italian",
+    "Beef",
+    "Ricotta",
+    "Cheese",
   ],
 };
 
-export default PizzaData;
+const SpecialtyPizza = {
+  Size: ['12"', '14"', '16"', '18"'],
+  Cost: ["16.00", "20.00", "23.00", "26.00"],
+  Pizza: {
+    "Joe's Fabulous Four":
+      "A classic pizza featuring savory Italian sausage, fresh mushrooms, onions and green peppers.",
+    "Joe's Vegetarian Supreme":
+      "Piled high with garden-fresh mushrooms, onions, green peppers, and sliced tomatoes.",
+    "Joe's Vegetarian Delight":
+      "Joe's combination of spinach, mushrooms, black olives, and sliced tomatoes will make this an instant favorite of any veggie lover.",
+    "Joe's BBQ Pizza":
+      "Sausage, crispy, bacon, and onions with Joe's zesty BBQ Sauce.",
+    "Honolulu Joe's":
+      "A delightful tropical twist of Canadian bacon and sweet onions.",
+    "Meaty Joe's":
+      "A mighty, meaty combination of pepperoni, sausage, ground beef, bacon, and canadian bacon.",
+    "Mama's White Pizza":
+      "Juicy grilled chicken and fresh spinach on Mama's homemade alfredo sauce.",
+    "Mama's Spicy White Pizza":
+      "Juicy grilled cajun chicken and fresh spinach on Mama's homemade alfredo sauce.",
+    "Joe's Cheezy Beef Pizza":
+      "Our Italian beef, sweet peppers, and mozzarella with garlic butter sauce.",
+    "Superbowl Pizza":
+      "Blackened Chicken, Buffalo Sauce, Crumbled Blue Cheese and Mozzarella.",
+    "Mama's Meatball Pizza": "With Hot or Sweet Peppers.",
+    "Chef Special Pizza":
+      "Roasted Whole Garlic and Butter Bottom, Artichoke, Mushroom and Spinach.",
+    "Pesto Grilled Chicken Pizza":
+      "Pesto & Grilled Chicken is the perfect answer to your pizza craving.",
+  },
+};
+
+const Calzone_Panzerotti = {
+  Calzone: [
+    "10.50",
+    "A pocket of dough generously filled with our homemade marinara and plenty of mozzarella. Baked until golden brown and delicious.",
+  ],
+  Panzerotti: [
+    "10.50",
+    "Mozzarella and zesty marinara are stuffed into a dough pocket and deep fried for tasty satisfaction.",
+  ],
+};
+
+export default { BuildYourOwnPizza, SpecialtyPizza, Calzone_Panzerotti }; 
