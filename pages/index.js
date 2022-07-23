4652134e6b31272a73888494cd334e7f2a2d7ce3
Ian Streator <istreator@yahoo.com>
2022-07-22 23:00:48 -0500

    cleaning and refactoring project code, touching up styling

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,69 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
 
-import Header from "../components/Header";
+import clsx from "clsx";
+import { useState } from "react";
+import { useIsMobile } from "../hooks/useIsMobile";
+
 import Information from "../components/Information";
 import Menu from "../components/Menu";
 
 export default function Home() {
-  const [mobile, setMobile] = useState(false);
+  const [theme, setTheme] = useState(false);
   const [view, setView] = useState("Menu");
   const changeView = () => {
     view === "Info" ? setView("Menu") : setView("Info");
   };
-
-  useEffect(() => {
-    const onResize = () => {
-      if (window.innerWidth < 1000) {
-        setMobile(true);
-      } else {
-        setMobile(false);
-      }
-    };
-    window.addEventListener("resize", onResize);
-
-    onResize();
-
-    return () => window.removeEventListener("resize", onResize);
-  }, []);
-
-  if (mobile) {
-    return (
-      <div className="app">
-        <Head>
-          <title>{"Joe's Pizza"}</title>
-          <meta
-            name="description"
-            content="Italian Pizza Restaurant In Schaumburg Illinois"
-          />
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            href="https://fonts.googleapis.com/css?family=Italianno"
-            rel="stylesheet"
-          ></link>
-        </Head>
-        <main className="main" id="theme">
-          <Header />
-
-          <div className="main_content">
-            <h1 className="title">{"Joe's Pizza"}</h1>
-
-            <div className={"menu_info"}>
-              {view === "Menu" ? <Information /> : <Menu />}
-            </div>
-            <button className="view_btn" onClick={changeView}>
-              {view}
-            </button>
-          </div>
-        </main>
-      </div>
-    );
-  } else {
-    return (
-      <div className="app">
-        <Head>
-          <title>{"Joe's Pizza"}</title>
-          <meta
-            name="description"
-            content="Italian Pizza Restaurant In Schaumburg Illinois"
-          />
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            href="https://fonts.googleapis.com/css?family=Italianno"
-            rel="stylesheet"
-          ></link>
-        </Head>
-        <main className="main" id="theme">
-          <Header />
-          <div className="main_content">
-            <Information />
-            <Menu />
-          </div>
-        </main>
-      </div>
+  const isMobile = useIsMobile();
+  let content = (
+    <>
+      <Information />
+      <Menu />
+    </>
+  );
+  if (isMobile) {
+    content = (
+      <>
+        <h1 key={"title"} className="title">
+          {"Joe's Pizza"}
+        </h1>
+        <div key={"view"} className={"menu_info"}>
+          {view === "Menu" ? <Information /> : <Menu />}
+        </div>
+        <button key={"btn"} className="view_btn" onClick={changeView}>
+          {view}
+        </button>
+      </>
     );
   }
+
+  return (
+    <div className="app">
+      <Head>
+        <title>{"Joe's Pizza"}</title>
+        <meta
+          name="description"
+          content="Italian Pizza Restaurant In Schaumburg Illinois"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Italianno"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <main className={clsx("main", theme && "dark")} id="theme">
+        <header className="header">
+          <div className="mobile">
+            <p>{"The Best Pizza In Schaumburg - Call Now: (847) 524-2204"}</p>
+            <p className="mobile_hide">{"|"}</p>
+            <p>{"Ordering must be done over the phone or in store."}</p>
+          </div>
+
+          <div className="toggle" id="toggle" onClick={() => setTheme(!theme)}>
+            <div className={clsx("icon", theme && "dark")}></div>
+          </div>
+        </header>
+        <div className="main_content">{content}</div>
+      </main>
+    </div>
+  );
 }
