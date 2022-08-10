6413058213630a3cd3efc7bead96e831b45be4c4
Ian Streator <istreator@yahoo.com>
2022-08-09 21:14:33 -0500

    built out pasta json data and worked on menu page WIP

--- components/menu/Beverages.jsx
+++ components/menu/Beverages.jsx
@@ -3,12 +3,12 @@ import BeveragesData from "../../data/Beverages";
 function Beverages() {
   return (
     <div className="main_container">
-      {BeveragesData.map(([type, cost]) => {
+      {BeveragesData.map(([type, price]) => {
         return (
           <div key={type} className="card" style={{ display: "flex" }}>
             <div className="card_header">
               <h1 className="name_cost">{`${type}`}</h1>
-              <p className="name_cost small">{`( $${cost.toFixed(2)} )`}</p>
+              <p className="name_cost small">{`( $${price.toFixed(2)} )`}</p>
             </div>
           </div>
         );
--- components/menu/Pasta.jsx
+++ components/menu/Pasta.jsx
@@ -1,7 +1,38 @@
+import Pastas from "../../data/PastasData";
+
 function Pasta() {
+  const dishes = Pastas.Pastas;
+  const toppers = Pastas.Toppers;
   return (
-    <h1>Pastas</h1>
-  )
+    <div className="main_container">
+      <h1 className="section_header">Toppers <p className="name_cost small">{`( $4.75 each )`}</p></h1>
+
+      <div className="small_container" style={{ flexDirection: "row", justifyContent:"space-around"}}>
+        {toppers.map((e) => {
+          return (
+            // <div className="card">
+              <h3 className="name_cost"> {e}</h3>
+            // </div>
+          );
+        })}
+      </div>
+      <h1 className="section_header">Entrées</h1>
+
+      <div className="small_container">
+        {dishes.map(([name, cost, description]) => {
+          return (
+            <div key={name} className="card">
+              <div className="card_header">
+                <h1 className="name_cost">{name}</h1>
+                <p className="name_cost small">{`( ${cost.toFixed(2)} )`} </p>
+              </div>
+              <p className="description">{description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Pasta
+export default Pasta;
